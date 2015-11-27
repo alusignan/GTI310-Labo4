dbb463341afcb8e8d8d420eee481f7cbbe671db8
alusignan <am08390@ens.etsmtl.ca>
2015-11-26 19:22:35 -0500

    ColorManager avec bonne valeurs

--- SqueezeLightMediaCodec/gti310/tp4/ColorManager.java
+++ SqueezeLightMediaCodec/gti310/tp4/ColorManager.java
@@ -38,26 +38,26 @@ public class ColorManager {
 		for (int i = 0; i < RGBimage[INDEX_NUMBER].length; i++) {
 			for (int j = 0; j < RGBimage[INDEX_NUMBER][INDEX_NUMBER].length; j++) {
 				
-				yCbCrImage[Main.Y][i][j] = 16 + ((int)(
-	                      65.738 * inputPPM[Main.R][i][j] + 
-	                      129.057 * inputPPM[Main.G][i][j] +
-	                       25.064 * inputPPM[Main.B][i][j]
+				yCbCrImage[Main.Y][i][j] = 16 + ((float)(
+	                      65.738 * RGBimage[Main.R][i][j] + 
+	                      129.057 * RGBimage[Main.G][i][j] +
+	                       25.064 * RGBimage[Main.B][i][j]
 	                    ));
 				
 				//(int)((inputPPM[Main.R][i][j] * 0.299)+(inputPPM[Main.G][i][j] * 0.587)+(inputPPM[Main.B][i][j] * 0.114));
 				
-				yCbCrImage[Main.Cb][i][j] = 128 + ((int)(
-	                     -37.945 * inputPPM[Main.R][i][j] + 
-	                     -74.494 * inputPPM[Main.G][i][j] +
-	                      112.439   * inputPPM[Main.B][i][j]
+				yCbCrImage[Main.Cb][i][j] = 128 + ((float)(
+	                     -37.945 * RGBimage[Main.R][i][j] + 
+	                     -74.494 * RGBimage[Main.G][i][j] +
+	                      112.439   * RGBimage[Main.B][i][j]
 	                   ));
 				
 				//(int)((128-(inputPPM[Main.R][i][j] * -0.168736)+(inputPPM[Main.G][i][j] * -0.331264)+(inputPPM[Main.B][i][j] * 0.5)));
 				
-				yCbCrImage[Main.Cr][i][j] = 128 + ((int)(
-	                     112.439    * inputPPM[Main.R][i][j] + 
-	                     -97.154 * inputPPM[Main.G][i][j] +
-	                     -18.285 * inputPPM[Main.B][i][j]
+				yCbCrImage[Main.Cr][i][j] = 128 + ((float)(
+	                     112.439    * RGBimage[Main.R][i][j] + 
+	                     -97.154 * RGBimage[Main.G][i][j] +
+	                     -18.285 * RGBimage[Main.B][i][j]
 	                   ));
 				
 				//(int)((128+(inputPPM[Main.R][i][j] * 0.5)+(inputPPM[Main.G][i][j] * -0.418688)+(inputPPM[Main.B][i][j] * -0.081312)));
@@ -68,9 +68,9 @@ public class ColorManager {
 				//yCbCrImage[Main.Cb][i][j] = (float)(128+((RGBimage[Main.R][i][j] * -0.168736)+(RGBimage[Main.G][i][j] * -0.331264)+(RGBimage[Main.B][i][j] * 0.5)));
 				//yCbCrImage[Main.Cr][i][j] = (float)(128+((RGBimage[Main.R][i][j] * 0.5)+(RGBimage[Main.G][i][j] * -0.418688)+(RGBimage[Main.B][i][j] * -0.081312)));
 				
-//				yCbCrImage[Main.Y][i][j] = (float) ((image[Main.R][i][j] * 0.299)+ (image[Main.G][i][j] * 0.587)+(image[Main.B][i][j] * 0.114));
-//				yCbCrImage[Main.Cb][i][j] = (float) (0.492*((image[Main.B][i][j])-(yCbCrImage[Main.Y][i][j])));
-//				yCbCrImage[Main.Cr][i][j] = (float) (0.877*((image[Main.R][i][j])-(yCbCrImage[Main.Y][i][j])));	
+//				yCbCrImage[Main.Y][i][j] = (float) ((RGBimage[Main.R][i][j] * 0.299)+ (RGBimage[Main.G][i][j] * 0.587)+(RGBimage[Main.B][i][j] * 0.114));
+//				yCbCrImage[Main.Cb][i][j] = (float) (0.492*((RGBimage[Main.B][i][j])-(yCbCrImage[Main.Y][i][j])));
+//				yCbCrImage[Main.Cr][i][j] = (float) (0.877*((RGBimage[Main.R][i][j])-(yCbCrImage[Main.Y][i][j])));	
 		}
 		return yCbCrImage;
 	}
@@ -85,27 +85,27 @@ public class ColorManager {
 		for (int i = 0; i < YCbCrImage[INDEX_NUMBER].length; i++) {
 			for (int j = 0; j < YCbCrImage[INDEX_NUMBER][INDEX_NUMBER].length; j++) {
 			
-				/*Trop de vert ??*/
-				R = inputPPM[Main.Y][i][j];// ((int) ( 298.082 * (inputPPM[Main.Y][i][j] - 16)   +
-                    //408.583 * (inputPPM[Main.Cr][i][j] - 128)    )) >> 8;
+			//Trop de vert...
+				R = ((int) ( 298.082 * (YCbCrImage[Main.Y][i][j] - 16)   +
+                    408.583 * (YCbCrImage[Main.Cr][i][j] - 128)    )) >> 8;
 						
-				//(int)(inputPPM[Main.Y][i][j] + 1.402 * (inputPPM[Main.Cr][i][j] - 128));
+				//(int)(YCbCrImage[Main.Y][i][j] + 1.402 * (YCbCrImage[Main.Cr][i][j] - 128));
 				
-				G = inputPPM[Main.Cb][i][j]; //((int) ( 298.082 * (inputPPM[Main.Y][i][j] - 16)   +
-                        //-100.291 * (inputPPM[Main.Cb][i][j] - 128) +
-                        //-208.120 * (inputPPM[Main.Cr][i][j] - 128)    )) >> 8;
+				G = ((int) ( 298.082 * (YCbCrImage[Main.Y][i][j] - 16)   +
+                        -100.291 * (YCbCrImage[Main.Cb][i][j] - 128) +
+                        -208.120 * (YCbCrImage[Main.Cr][i][j] - 128)    )) >> 8;
 						
-				//(int)(inputPPM[Main.Y][i][j] - 0.34414 * ((inputPPM[Main.Cb][i][j] - 128)) - 0.71414 * ((inputPPM[Main.Cr][i][j] - 128)));
+				//(int)(YCbCrImage[Main.Y][i][j] - 0.34414 * ((YCbCrImage[Main.Cb][i][j] - 128)) - 0.71414 * ((inputPPM[Main.Cr][i][j] - 128)));
 				
-				B = inputPPM[Main.Cr][i][j]; //((int) ( 298.082 * (inputPPM[Main.Y][i][j] - 16)   +
-                        //516.411 * (inputPPM[Main.Cb][i][j] - 128)    )) >> 8;
+				B = ((int) ( 298.082 * (YCbCrImage[Main.Y][i][j] - 16)   +
+                        516.411 * (YCbCrImage[Main.Cb][i][j] - 128)    )) >> 8;
 						
-				//(int)(inputPPM[Main.Y][i][j] + 1.772 * (inputPPM[Main.Cb][i][j] - 128));
+				//(int)(YCbCrImage[Main.Y][i][j] + 1.772 * (YCbCrImage[Main.Cb][i][j] - 128));
 				
 				/*Trop de rouge ? bleu ? image mauve*/
-				//R = (int)(1.140250855*(inputPPM[Main.Cr][i][j]+0.877*inputPPM[Main.Y][i][j]));
-                //G = (int)(-0.3947313749*(inputPPM[Main.Cb][i][j]+1.471403709*(inputPPM[Main.Cr][i][j]-1.721795786*inputPPM[Main.Y][i][j])));
-                //B = (int)(2.032520325*(inputPPM[Main.Cb][i][j]+0.492*inputPPM[Main.Y][i][j]));
+				//R = (int)(1.140250855*(YCbCrImage[Main.Cr][i][j]+0.877*YCbCrImage[Main.Y][i][j]));
+                //G = (int)(-0.3947313749*(YCbCrImage[Main.Cb][i][j]+1.471403709*(YCbCrImage[Main.Cr][i][j]-1.721795786*inputPPM[Main.Y][i][j])));
+                //B = (int)(2.032520325*(YCbCrImage[Main.Cb][i][j]+0.492*YCbCrImage[Main.Y][i][j]));
 				
 				/*Trop de rouge ??*/
 //				R = (int)(YCbCrImage[Main.Y][i][j] + 1.402 * (YCbCrImage[Main.Cr][i][j] - 128));
@@ -113,9 +113,9 @@ public class ColorManager {
 //				B = (int)(YCbCrImage[Main.Cr][i][j] + 1.772 * (YCbCrImage[Main.Cb][i][j] - 128));
 				
 				//Validation inspiré de : http://stackoverflow.com/questions/4041840/function-to-convert-ycbcr-to-rgb
-                R = (R<0 ? 0 :( R > 255 ? 255 : R) );
-                G = (G<0 ? 0 :( G > 255 ? 255 : G) );
-                B = (B<0 ? 0 :( B > 255 ? 255 : B) );
+//                R = (R<0 ? 0 :( R > 255 ? 255 : R) );
+//                G = (G<0 ? 0 :( G > 255 ? 255 : G) );
+//                B = (B<0 ? 0 :( B > 255 ? 255 : B) );
                 
 				RGBimage[Main.R][i][j] = R;
 				RGBimage[Main.G][i][j] = G;	
